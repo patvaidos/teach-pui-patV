53623443e16f83bdb9577cb62e92a220cb713de5
patvaidos <75406701+patvaidos@users.noreply.github.com>
2022-09-21 11:34:06 -0400

    More changes added to js -- close to functinoal

--- solution-hw3/price.js
+++ solution-hw3/price.js
@@ -1,11 +1,47 @@
+// define constants
 const basePrice = 2.49;
 const glazingPriceVariables = [0, 0, 0.5, 1.5];
-packingSizePrices = [1, 3, 5, 10];
-//
-function choosePrice() {}
+const packingSizePrices = [1, 3, 5, 10];
+
+// retrieve elements from HTML dropdown menus
+const dropdown = document.getElementsByClassName("dropdown_content");
+console.log(dropdown);
+const glazing = dropdown[0];
+const packSize = dropdown[1];
+
+// event handler
+
+function chooseGlazingPrice() {
+  console.log(glazing);
+}
+
+function choosePackPrice() {
+  console.log(packSize);
+}
+
+chooseGlazingPrice();
+choosePackPrice();
+//bs class definition/constructor
+// class Roll {
+//   rollName;
+//   glazingPrice;
+//   packingSizePrice;
+// }
+
+// constructor(rollName, glazingPrice, packingSizePrice); {
+//   this.rollName = rollName;
+//   this.glazingPrice = glazingPrice;
+//   this.packingSizePrice = packingSizePrice;
+// };
+
+// total price change
+let totalPrice = document.querySelector(".add-to-cart");
 
 function updatePrice(glazingPriceVariable, n) {
-  newPrice = (basePrice + glazingPriceVariable) * packingSizePrices[n];
+  let newPrice = (basePrice + glazingPriceVariable) * packingSizePrices[n];
   console.log(newPrice);
+  document.getElementById;
   return newPrice;
 }
+
+// totalPrice.textContent = updatePrice();
