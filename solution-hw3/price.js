affe73c39f0929f3424ddb40b3db1e5b4b3cecda
patvaidos <75406701+patvaidos@users.noreply.github.com>
2022-09-21 12:51:46 -0400

    Code is able to extract the options from the HTML dropdown menu, working on retrieving the value

--- solution-hw3/price.js
+++ solution-hw3/price.js
@@ -4,23 +4,37 @@ const glazingPriceVariables = [0, 0, 0.5, 1.5];
 const packingSizePrices = [1, 3, 5, 10];
 
 // retrieve elements from HTML dropdown menus
-const dropdown = document.getElementsByClassName("dropdown_content");
-console.log(dropdown);
-const glazing = dropdown[0];
-const packSize = dropdown[1];
+// const dropdown =
+// console.log(dropdown);
+const glazingOptions = document.getElementById("Glazing Options");
+// glazingOptions.addEventListener("click", onChange());
 
+const packSizeOptions = document.getElementById("Pack Size");
+// packSizeOptions.addEventListener("click", onChange());
+console.log(glazingOptions);
+console.log(packSizeOptions);
 // event handler
 
-function chooseGlazingPrice() {
-  console.log(glazing);
+function onChange() {
+  //change option attribute to selected
+  value = glazingOptions.getElementById("");
+  index = glazingOptions.index;
+  text = glazingOptions.text;
+  let list = [value, index, text];
+  return list;
 }
+
+//onChange or onClick??
+glazingOptions.onchange = onChange();
+console.log(onChange());
 
 function choosePackPrice() {
   console.log(packSize);
 }
 
-chooseGlazingPrice();
-choosePackPrice();
+// chooseGlazingPrice();
+// choosePackPrice();
+
 //bs class definition/constructor
 // class Roll {
 //   rollName;
