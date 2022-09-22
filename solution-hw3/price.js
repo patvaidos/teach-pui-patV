eb5b37017900e3d6cd20717b77298572261b701d
patvaidos <75406701+patvaidos@users.noreply.github.com>
2022-09-22 14:59:21 -0400

    Changed dropdown code
    Interface now displays dropdown menu options

--- solution-hw3/price.js
+++ solution-hw3/price.js
@@ -1,61 +1,108 @@
 // define constants
+class Product {
+  priceChange;
+  optionName;
+
+  constuctor(priceChange, optionName) {
+    this.priceChange = priceChange;
+    this.optionName = optionName;
+  }
+}
 const basePrice = 2.49;
-const glazingPriceVariables = [0, 0, 0.5, 1.5];
-const packingSizePrices = [1, 3, 5, 10];
 
-// retrieve elements from HTML dropdown menus
-// const dropdown =
-// console.log(dropdown);
-const glazingOptions = document.getElementById("Glazing Options");
-// glazingOptions.addEventListener("click", onChange());
+let glazingSelect = [
+  {
+    optionName: "Keep original",
+    priceChange: 0.0,
+  },
+  {
+    optionName: "Sugar milk",
+    priceChange: 0.0,
+  },
+  {
+    optionName: "Vanilla milk",
+    priceChange: 0.5,
+  },
+  {
+    optionName: "Double chocolate",
+    priceChange: 1.5,
+  },
+];
 
-const packSizeOptions = document.getElementById("Pack Size");
-// packSizeOptions.addEventListener("click", onChange());
-console.log(glazingOptions);
-console.log(packSizeOptions);
-// event handler
-
-function onChange() {
-  //change option attribute to selected
-  value = glazingOptions.getElementById("");
-  index = glazingOptions.index;
-  text = glazingOptions.text;
-  let list = [value, index, text];
-  return list;
-}
-
-//onChange or onClick??
-glazingOptions.onchange = onChange();
-console.log(onChange());
-
-function choosePackPrice() {
-  console.log(packSize);
-}
-
-// chooseGlazingPrice();
-// choosePackPrice();
-
-//bs class definition/constructor
-// class Roll {
-//   rollName;
-//   glazingPrice;
-//   packingSizePrice;
+let packSelectArray = [
+  {
+    optionName: 1,
+    priceChange: 1,
+  },
+  {
+    optionName: 3,
+    priceChange: 3,
+  },
+  {
+    optionName: 6,
+    priceChange: 5,
+  },
+  {
+    optionName: 12,
+    priceChange: 10,
+  },
+];
+// function displayOption(option) {
+//   console.log(option);
+//   let optionDisplay = document.querySelector("#Glazing-Options");
+//   console.log(optionDisplay);
+//   optionDisplay.innerText = option.optionName;
 // }
 
-// constructor(rollName, glazingPrice, packingSizePrice); {
-//   this.rollName = rollName;
-//   this.glazingPrice = glazingPrice;
-//   this.packingSizePrice = packingSizePrice;
-// };
+function glazingValueChange() {
+  console.log(selectGlazing);
+  console.log("You selected (glazing)" + selectGlazing.value);
 
-// total price change
-let totalPrice = document.querySelector(".add-to-cart");
+  let optionIndex = parseInt(selectGlazing.value);
 
-function updatePrice(glazingPriceVariable, n) {
-  let newPrice = (basePrice + glazingPriceVariable) * packingSizePrices[n];
-  console.log(newPrice);
-  document.getElementById;
-  return newPrice;
+  let optionToDisplay = glazingSelect[optionIndex];
+
+  displayOption(optionToDisplay);
 }
 
-// totalPrice.textContent = updatePrice();
+function packValueChange() {
+  console.log(packSelect);
+  console.log("You selected (packValue)" + packSelect.value);
+
+  let optionIndex = parseInt(packSelect.value);
+
+  let optionToDisplay = packSelect[optionIndex];
+
+  displayOption(optionToDisplay);
+}
+
+let selectGlazing = document.querySelector("#Glazing-Options");
+let packSelect = document.querySelector("#Pack-Size");
+
+console.log(selectGlazing);
+console.log(packSelect);
+function createGlazingDropdown() {
+  for (i = 0; i < 4; ++i) {
+    //create an HTML element, choose from array of options using loop index, add to certain menu and display
+    var option = document.createElement("option");
+    option.text = glazingSelect[i].optionName;
+    option.value = glazingSelect[i].priceChange;
+    selectGlazing.add(option);
+  }
+  selectGlazing.addEventListener("change", glazingValueChange);
+}
+
+function createPackDropdown() {
+  for (i = 0; i < 4; ++i) {
+    //create an HTML element, choose from array of options using loop index, add to certain menu and display
+    var option = document.createElement("option");
+    console.log(option);
+    option.text = packSelectArray[i].optionName;
+    option.value = packSelectArray[i].priceChange;
+    packSelect.add(option);
+  }
+  packSelect.addEventListener("change", packValueChange);
+}
+
+createGlazingDropdown();
+createPackDropdown();
