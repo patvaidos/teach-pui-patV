584afb37c537e6b718d8903fa4d3aade2b8c66bc
patvaidos <75406701+patvaidos@users.noreply.github.com>
2022-10-24 14:33:43 -0400

    Began HW 6

---
+++ solution-hw6/cartFunctionality.js
@@ -0,0 +1,194 @@
+//This file manages all the functionality for
+//Map of glazing names and price changes
+const glazingMap = {
+  Original: 0,
+  "Sugar milk": 0,
+  "Vanilla milk": 0.5,
+  "Double chocolate": 1.5,
+};
+
+//Map of pack sizes and price changes
+const packMap = {
+  1: 1,
+  3: 3,
+  6: 5,
+  12: 10,
+};
+
+//Class definition for products added to the cart.
+class CartItem {
+  rollType;
+  rollGlazing;
+  packSize;
+  basePrice;
+  imageURL;
+  element;
+
+  constructor(rollType, rollGlazing, packSize, basePrice, imageURL) {
+    this.rollType = rollType;
+    this.rollGlazing = rollGlazing;
+    this.packSize = packSize;
+    this.basePrice = basePrice;
+    this.imageURL = imageURL;
+
+    //This calls the calculatePrice() function later in the document and uses the mapped glazing and pack size price changes
+    this.calculatedPrice = calculatePrice(
+      this.basePrice,
+      glazingMap[this.rollGlazing],
+      packMap[this.packSize]
+    );
+  }
+
+  createElement(deleteFunction) {
+    //Copies the template from the HTML page to create HTML elements on the page.
+    console.log("Adding item");
+    let template = document.querySelector("#cart-item-template");
+    console.log(template);
+    let clone = template.content.cloneNode(true);
+    this.element = clone.querySelector(".child-3-1");
+    console.log("this.element: " + this.element);
+
+    let container = document.querySelector(".container-3");
+    container.prepend(this.element);
+
+    let deleteButton = this.element.querySelector(".remove-button");
+    deleteButton.addEventListener("click", () => {
+      deleteFunction(".child-3-1");
+    });
+
+    this.updateCart();
+  }
+
+  updateCart() {
+    //Updates the elements on the page to reflect roll item information
+    let image = document.querySelector(".productimg");
+    console.log(image);
+    image.src = this.imageURL;
+
+    let name = document.querySelector("#title");
+    name.innerHTML = this.rollType + " Cinnamon Roll";
+
+    let price = document.querySelector("#price");
+    price.innerHTML = formatter.format(this.calculatedPrice);
+
+    let glazing = document.querySelector("#glazing");
+    glazing.innerHTML = this.rollGlazing;
+
+    let packsize = document.querySelector("#packSize");
+    packsize.innerHTML = "Pack Size: " + this.packSize;
+
+    saveToLocalStorage();
+  }
+}
+
+//Cart Set
+// let cart = new Set();
+
+//Populate cart with items
+// let original = new CartItem(
+//   "Original",
+//   "Sugar milk",
+//   1,
+//   2.49,
+//   "products/original-cinnamon-roll.jpg"
+// );
+// cart.add(original);
+
+// let walnut = new CartItem(
+//   "Walnut",
+//   "Vanilla milk",
+//   12,
+//   3.49,
+//   "products/walnut-cinnamon-roll.jpg"
+// );
+// cart.add(walnut);
+
+// let raisin = new CartItem(
+//   "Raisin",
+//   "Sugar milk",
+//   3,
+//   2.99,
+//   "products/raisin-cinnamon-roll.jpg"
+// );
+// cart.add(raisin);
+
+// let apple = new CartItem(
+//   "Apple",
+//   "Original",
+//   3,
+//   3.49,
+//   "products/apple-cinnamon-roll.jpg"
+// );
+// cart.add(apple);
+
+function addToCart(roll) {
+  //Adds the current product to the global cart array
+  console.log("Add to cart function passed");
+  roll.createElement(deleteItem);
+  // cart.add(roll);
+  saveToLocalStorage();
+}
+
+function deleteItem(item) {
+  //Deletes item from cart
+  item.element.remove();
+  cart.delete(item);
+  saveToLocalStorage();
+}
+
+function cartFill() {
+  //Function to populate cart
+  for (let item of cart) {
+    item.createElement(() => {
+      deleteItem(item);
+    });
+  }
+}
+
+function updateTotal() {
+  //Updates the total price listed on the page.
+  let total = 0;
+  for (let item of cart) {
+    total = total + item.calculatedPrice;
+  }
+  let price = document.querySelector("#total");
+  price.innerHTML = formatter.format(total);
+}
+
+function calculatePrice(basePrice, glazingPrice, packPrice) {
+  //Calculates the total price of the products.
+  return (basePrice + glazingPrice) * packPrice;
+}
+
+//Formats the finalPrice answer into a currency format.
+var formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+cartFill();
+updateTotal();
+
+//Homework 6 Local Storage
+
+function saveToLocalStorage() {
+  const cartArray = Array.from(cart);
+  console.log(cart);
+  const cartArrayString = JSON.stringify(cartArray);
+  console.log(cartArrayString);
+
+  localStorage.setItem("storedItems", cartArrayString);
+}
+
+function retrieveFromLocalStorage() {
+  const cartArrayString = localStorage.getItem("storedItems");
+  const cartArray = JSON.parse(cartArrayString);
+  for (const cartData of cartArray) {
+    const cart = addToCart(cartData);
+    createElement(cart);
+  }
+}
+
+if (localStorage.getItem("storedItems") != null) {
+  retrieveFromLocalStorage();
+}
---
+++ solution-hw6/price.js
@@ -0,0 +1,180 @@
+//This JS file contains functionality for changing the product on the product details page (product.html) and updating the cart.
+//Homework 3 and 4
+
+class Roll {
+  //The class Roll contains the product name (rollType), the glazing option (rollGlazing), the pack size (packSize), the base price (basePrice),
+  //and the image URL directory (imageURL)
+  rollType;
+  rollGlazing;
+  packSize;
+  basePrice;
+  imageURL;
+  calculatedPrice;
+
+  constructor(rollType, rollGlazing, packSize, basePrice, imageURL) {
+    this.rollType = rollType;
+    this.rollGlazing = rollGlazing;
+    this.packSize = packSize;
+    this.basePrice = basePrice;
+    this.imageURL = imageURL;
+  }
+
+  updateElement() {
+    //This function updates the DOM with the chosen object's properties (name, image, and price)
+    let headerElement = document.querySelector(".product-subheading h2");
+    headerElement.innerHTML = this.rollType + " Cinnamon Roll";
+
+    let rollImage = document.querySelector(".productimg");
+    rollImage.src = "products/" + this.imageURL;
+
+    let rollPrice = document.querySelector("#total");
+    rollPrice.innerHTML = "$" + this.basePrice;
+  }
+}
+
+//Global variables are defined here
+let currentProduct;
+const basePrice = 2.49;
+let currentProductGlazing;
+let currentProductPack;
+
+//Default indexes for the glazing and pack inventories
+let glazingIndex = 0;
+let packIndex = 0;
+
+//Array of objects to be added/removed from cart.
+let cart = [];
+
+//Arrays of Product objects glazing and packs, with attributes optionName, the name of the product and the price change applied to the base price.
+let glazingArray = [
+  {
+    optionName: "Keep original",
+    priceChange: 0.0,
+  },
+  {
+    optionName: "Sugar milk",
+    priceChange: 0.0,
+  },
+  {
+    optionName: "Vanilla milk",
+    priceChange: 0.5,
+  },
+  {
+    optionName: "Double chocolate",
+    priceChange: 1.5,
+  },
+];
+
+let packArray = [
+  {
+    optionName: 1,
+    priceChange: 1,
+  },
+  {
+    optionName: 3,
+    priceChange: 3,
+  },
+  {
+    optionName: 6,
+    priceChange: 5,
+  },
+  {
+    optionName: 12,
+    priceChange: 10,
+  },
+];
+
+//-----------------Logic for adjusting price based on Glazing and Pack size choice. User can click on the-----------------------------------
+//-------glazing and pack size options for their order using the dropdown menu and the price will update based on choice.--------------------
+
+//Variables store the selected HTML element for glazing and pack size respectfully.
+let selectGlazing = document.querySelector("#Glazing-Options");
+let packSelect = document.querySelector("#Pack-Size");
+
+function glazingValueChange() {
+  //Takes the glazing value from HTML element and calculates the index to retrieve the value. Calculates price from index.
+
+  glazingIndex = parseInt(selectGlazing.value);
+
+  calculatePrice(glazingIndex, 0);
+  currentProductGlazing = glazingIndex;
+}
+
+function packValueChange() {
+  //Takes the pack value from HTML element and calculates the index to retrieve the value. Calculates price from index.
+
+  packIndex = parseInt(packSelect.value);
+
+  calculatePrice(0, packIndex);
+  currentProductPack = packIndex;
+}
+
+function createGlazingDropdown() {
+  //Populates the glazing options dropdown menu with glazing objects.
+  for (i = 0; i < 4; ++i) {
+    var option = document.createElement("option");
+    option.text = glazingArray[i].optionName;
+    option.value = i;
+    selectGlazing.add(option);
+  }
+  selectGlazing.addEventListener("change", glazingValueChange);
+}
+
+function createPackDropdown() {
+  //Populates the pack options dropdown menu with glazing objects.
+  for (i = 0; i < 4; ++i) {
+    var option = document.createElement("option");
+    option.text = packArray[i].optionName;
+    option.value = i;
+    packSelect.add(option);
+  }
+  packSelect.addEventListener("change", packValueChange);
+}
+
+function calculatePrice(glazing, pack) {
+  //Calculates the final price of the purchase by using a price formula with variables from the glazing & pack size arrays.
+  let finalPrice = currentProduct.basePrice;
+  let selectedGlaze = glazingArray[glazingIndex].priceChange;
+  let selectedPack = packArray[packIndex].priceChange;
+  finalPrice = (currentProduct.basePrice + selectedGlaze) * selectedPack;
+  finalPrice = finalPrice.toString();
+  document.querySelector("#total").innerText = formatter.format(finalPrice);
+}
+
+//Formats the finalPrice answer into a currency format.
+var formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+//Function calls for dynamically populating the dropdown menus
+createGlazingDropdown();
+createPackDropdown();
+
+//------------------Here is the code for updating the DOM elements to match the user choice from the gallery menu.------------------------------
+function parseProducts() {
+  //Function that takes user input from Gallery page and creates a new Roll object and check choice against rollsData.js.
+  //Defines the currentProduct global variable to remember choice for adding to cart.
+  const queryString = window.location.search;
+  const params = new URLSearchParams(queryString);
+  const chosenRoll = params.get("roll");
+
+  let instanceProduct;
+  rolls[chosenRoll];
+  instanceProduct = new Roll(
+    chosenRoll,
+    currentProductGlazing,
+    currentProductPack,
+    rolls[chosenRoll].basePrice,
+    rolls[chosenRoll].imageFile
+  );
+  instanceProduct.updateElement();
+  console.log("Product base price: " + rolls[chosenRoll].basePrice);
+  currentProduct = instanceProduct;
+}
+
+//Function call to parse products.
+parseProducts();
+
+// //Add to cart element
+let cartElement = document.getElementById("add_to_cart");
